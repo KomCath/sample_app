796e3f30bc4bb749cf9de0f130202dd679657543
Catharina Komrij <komcath@gmail.com>
2021-03-03 10:20:43 -0800

    Finish user edit, update, index and destroy actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,12 +4,14 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by(email: params[:session][:email])
+    @user = User.find_by(email: params[:session][:email].downcase)
     if @user &.authenticate(params[:session][:password])
+      forwarding_url = session[:forwarding_url]
       reset_session
       params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
       log_in @user
-      redirect_to @user
+      # redirect_back_or @user
+      redirect_to forwarding_url || @user
     else
     flash.now[:danger] = "Invalid email/password combination"
     render 'new'
@@ -20,5 +22,4 @@ class SessionsController < ApplicationController
     log_out if logged_in?
     redirect_to root_url
   end
-
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -15,6 +15,7 @@ module SessionsHelper
     cookies.permanent[:remember_token] = user.remember_token
   end
 
+
   # Returns the current logged-in user (if any).
   def current_user
     if (user_id = session[:user_id])
@@ -29,6 +30,11 @@ module SessionsHelper
         @current_user = user
       end
     end
+  end
+
+  # Returns true if given user is the current user.
+  def current_user?(user)
+    user && user == current_user
   end
 
   # Returns true if the user is logged in, false otherwise.
@@ -48,5 +54,16 @@ module SessionsHelper
     forget(current_user)
     reset_session
     @current_user = nil
+  end
+
+  # Redirect to stored location (or to the defalut).
+  # def redirect_back_or(default)
+  #   redirect_to(session[:forwarding_url] || default)
+  #   session.delete(:forwarding_url)
+  # end
+
+  # Stores the URL trying to be accessed.
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
   end
 end
